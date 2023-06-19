46a73b835a26106dc0a220aeceda82f22519cf03
umidjonburkhonov <umedburxonov99@gmail.com>
2023-06-19 14:36:36 +0200

    muster-portfolio

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// "use scrict ";
+
+// const umbu = document.querySelector(".main-section__introduction");
+// olibTashla = document.querySelectorAll(".services__cards img");
+// orqaFon = document.querySelector(".main-banner");
+// headerList = document.querySelector(".main-navigation__items");
+
+// olibTashla.forEach((item) => {
+//   item.remove();
+// });
+
+// umbu.textContent = "Kel buyoqqa";
+
+// orqaFon.style.backgroundImage = "url(Images/blog-1.jpg)";
+
+// const headListOf = {
+//   headListOnn: ["u", "b", "k", "l", "z", "p", "r", "w", "j"],
+// };
+
+// headerList.innerHTML = "";
+
+// headListOf.headListOnn.forEach((item, idx) => {
+//   headerList.innerHTML += ` <li class="main-navigation__item active">
+//     ${idx + 1} ${item}
+// </li>`;
+// });
